5779286887c5dbc4f666863d34ccb1789d1181d9
Anuj Parashar <anujparashar@microsoft.com>
2023-07-13 17:52:47 +1000

    Updated scripts to include Purview deployment

--- byos/modern-data-warehousing/deploy/BYOS-deployAll.ps1
+++ byos/modern-data-warehousing/deploy/BYOS-deployAll.ps1
@@ -1,25 +1,35 @@
 
 
 param(
-    [string]$teamCount = "1",
+    [string]$teamCount = "5",
     [string]$DeploymentTemplateFile = "$PSScriptRoot\ARM\DeployMDWOpenHackLab.json",
     [string]$DeploymentParameterFile = "$PSScriptRoot\ARM\DeployMDWOpenHackLab.parameters.json",
-    [string]$Location = "eastus",
+    [string]$Location = "westus3",
     [securestring]$SqlAdminLoginPassword,
     [securestring]$VMAdminPassword
 )
 $teamCount = Read-Host "How many teams are hacking?";
-$region = Read-Host "What Region Resources be deployed to (i.e. centralus, southcentralus, japaneast, etc)?";
 
-for ($i = 1; $i -le $teamCount; $i++)
+for ($i = 0; $i -le $teamCount; $i++)
 {
     $teamName = $i;
     if ($i -lt 10)
     {
         $teamName = "0" + $i;
     }
-    
-    $resourceGroupName = "mdw-oh-" + $teamName + "-" + $region
+
+    $region = switch($i)
+        {
+            0 {"australiaeast"}
+            1 {"australiaeast"}
+            2 {"australiaeast"}
+            3 {"centralindia"}
+            4 {"centralindia"}
+            5 {"centralindia"}
+        }
+
+    $purviewName = "pview-team-" + $teamName
+    $resourceGroupName = "rg-fabric-oh-team-" + $teamName
     $deploymentName = "azuredeploy" + "-" + (Get-Date).ToUniversalTime().ToString('MMdd-HHmmss')
     Write-Host("Now deploying RG to " + $resourceGroupName);
 
@@ -29,5 +39,5 @@ for ($i = 1; $i -le $teamCount; $i++)
     if (!$resourceGroup) {
         $resourceGroup = New-AzResourceGroup -Name $ResourceGroupName -Location $region
     }
-    New-AzResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -Location $region -TemplateFile $DeploymentTemplateFile -TemplateParameterFile $DeploymentParameterFile -sqlAdminLoginPassword $SqlAdminLoginPassword -VMAdminPassword $VMAdminPassword -AsJob    
+    New-AzResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -Location $region -TemplateFile $DeploymentTemplateFile -TemplateParameterFile $DeploymentParameterFile -purviewName $purviewName -sqlAdminLoginPassword $SqlAdminLoginPassword -VMAdminPassword $VMAdminPassword -AsJob    
 }
